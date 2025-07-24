107b260d85d8adb334523b6833d3e5fe6cd4aa0d
500a5 <500a5@mail.ru>
2025-07-23 20:29:47 -0700

    fix scope

--- app/src/main/java/soft/divan/financemanager/App.kt
+++ app/src/main/java/soft/divan/financemanager/App.kt
@@ -2,7 +2,18 @@ package soft.divan.financemanager
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.cancel
+import javax.inject.Inject
 
 @HiltAndroidApp
 class App: Application() {
+
+    @Inject
+    lateinit var applicationScope: CoroutineScope
+
+    override fun onTerminate() {
+        super.onTerminate()
+        applicationScope.cancel()
+    }
 }
