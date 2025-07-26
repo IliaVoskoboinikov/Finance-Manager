21a88e7e7180a17970206fec822456973e9eed18
500a5 <500a5@mail.ru>
2025-07-25 21:14:18 -0700

    init theme

--- core/uikit/src/main/java/soft/divan/financemanager/uikit/theme/Color.kt
+++ core/uikit/src/main/java/soft/divan/financemanager/uikit/theme/Color.kt
@@ -25,3 +25,16 @@ val CoralRed = Color(0xFFE46962)
 
 
 val SoftLavender = Color(0xFFF7F2FA)
+
+
+val Purple80Dark = Color(0xFFCF6679)
+val NeonMintDark = Color(0xFF00C57A)
+val MintBackgroundDark = Color(0xFF1A3C2D)
+val BlackDark = Color(0xFF121212)
+val CharcoalPurpleDark = Color(0xFFE6E1E5)
+val WhiteDark = Color(0xFFFFFFFF)
+val GraphiteDark = Color(0xFFCAC4D0)
+val LavenderMistDark = Color(0xFF2A2A2E)
+val RoseWhiteDark = Color(0xFF2D1B2F)
+val CoralRedDark = Color(0xFFB3261E)
+val SoftLavenderDark = Color(0xFF3E2C46)
--- feature/settings/settings-impl/src/main/java/soft/divan/financemanager/feature/settings/settings_impl/screens/SettingsScreen.kt
+++ feature/settings/settings-impl/src/main/java/soft/divan/financemanager/feature/settings/settings_impl/screens/SettingsScreen.kt
@@ -11,12 +11,16 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import soft.divan.financemanager.feature.settings.settings_impl.viewModel.SettingsViewModel
 import soft.divan.financemanager.string.R
 import soft.divan.financemanager.uikit.components.ContentTextListItem
 import soft.divan.financemanager.uikit.components.FMDriver
@@ -30,13 +34,57 @@ import soft.divan.financemanager.uikit.theme.FinanceManagerTheme
 @Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
 @Composable
 fun SettingsScreenPreview() {
-    FinanceManagerTheme {
+    FinanceManagerTheme(true) {
         SettingsScreen(navController = rememberNavController())
     }
 }
 
 @Composable
-fun SettingsScreen(modifier: Modifier = Modifier, navController: NavController) {
+fun SettingsScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController,
+    viewModel: SettingsViewModel = hiltViewModel()
+) {
+
+    val isDarkTheme by viewModel.isDarkThemeEnabled.collectAsState()
+
+    val settingsItems = listOf(
+        SettingsListItemModel.WithSwitch(
+            title = R.string.dark_theme,
+            isChecked = isDarkTheme,
+            onToggle = { isChecked -> viewModel.toggleTheme(!isDarkTheme) }
+        ),
+        SettingsListItemModel.WithArrow(
+            title = R.string.primary_color,
+            onClick = { /* handle */ }
+        ),
+        SettingsListItemModel.WithArrow(
+            title = R.string.sounds,
+            onClick = { /* handle */ }
+        ),
+        SettingsListItemModel.WithArrow(
+            title = R.string.haptics,
+            onClick = { /* handle */ }
+        ),
+        SettingsListItemModel.WithArrow(
+            title = R.string.passcode,
+            onClick = { /* handle */ }
+        ),
+        SettingsListItemModel.WithArrow(
+            title = R.string.synchronization,
+            onClick = { /* handle */ }
+        ),
+        SettingsListItemModel.WithArrow(
+            title = R.string.language,
+            onClick = { /* handle */ }
+        ),
+        SettingsListItemModel.WithArrow(
+            title = R.string.program_notes,
+            onClick = { /* handle */ }
+        )
+
+    )
+
     Box(modifier = modifier.fillMaxSize()) {
         Column(modifier = Modifier.fillMaxSize()) {
             TopBar(
@@ -51,45 +99,9 @@ fun SettingsScreen(modifier: Modifier = Modifier, navController: NavController) 
         }
 
     }
+
+
 }
-
-private val settingsItems = listOf(
-    SettingsListItemModel.WithSwitch(
-        title = R.string.dark_theme,
-        isChecked = false,
-        onToggle = { isChecked -> /* handle */ }
-    ),
-    SettingsListItemModel.WithArrow(
-        title = R.string.primary_color,
-        onClick = { /* handle */ }
-    ),
-    SettingsListItemModel.WithArrow(
-        title = R.string.sounds,
-        onClick = { /* handle */ }
-    ),
-    SettingsListItemModel.WithArrow(
-        title = R.string.haptics,
-        onClick = { /* handle */ }
-    ),
-    SettingsListItemModel.WithArrow(
-        title = R.string.passcode,
-        onClick = { /* handle */ }
-    ),
-    SettingsListItemModel.WithArrow(
-        title = R.string.synchronization,
-        onClick = { /* handle */ }
-    ),
-    SettingsListItemModel.WithArrow(
-        title = R.string.language,
-        onClick = { /* handle */ }
-    ),
-    SettingsListItemModel.WithArrow(
-        title = R.string.program_notes,
-        onClick = { /* handle */ }
-    )
-
-)
-
 sealed interface SettingsListItemModel {
     data class WithSwitch(
         val title: Int,
