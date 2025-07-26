99280d410eedf09b66ca12e1c1f22caadb770620
500a5 <500a5@mail.ru>
2025-07-26 00:52:19 -0700

    init colors

--- feature/settings/settings-impl/src/main/java/soft/divan/financemanager/feature/settings/settings_impl/navigation/SettingsFeatureImpl.kt
+++ feature/settings/settings-impl/src/main/java/soft/divan/financemanager/feature/settings/settings_impl/navigation/SettingsFeatureImpl.kt
@@ -4,14 +4,21 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
+import androidx.navigation.navigation
 
 import soft.divan.financemanager.feature.settings.settings_api.SettingsFeatureApi
+import soft.divan.financemanager.feature.settings.settings_impl.screens.ColorSelectionScreen
 import soft.divan.financemanager.feature.settings.settings_impl.screens.SettingsScreen
 import javax.inject.Inject
+
+private const val baseRoute = "settings"
+private const val scenarioSettingsRoute = "${baseRoute}/scenario"
+private const val screenSettingsColorRoute = "$scenarioSettingsRoute/color"
 
 class SettingsFeatureImpl @Inject constructor() : SettingsFeatureApi {
 
     override val route: String = "settings"
+
 
     override fun registerGraph(
         navGraphBuilder: NavGraphBuilder,
@@ -21,8 +28,26 @@ class SettingsFeatureImpl @Inject constructor() : SettingsFeatureApi {
         navGraphBuilder.composable(route) {
             SettingsScreen(
                 modifier = modifier,
-                navController = navController
+                navController = navController,
+                onNavigateToColor = {
+                    navController.navigate(screenSettingsColorRoute)
+                },
             )
+        }
+
+        /* Nested graph for internal scenario */
+        navGraphBuilder.navigation(
+            route = scenarioSettingsRoute,
+            startDestination = screenSettingsColorRoute
+        ) {
+
+            composable(route = screenSettingsColorRoute) {
+                ColorSelectionScreen(
+                    modifier = modifier,
+                    navController = navController
+                )
+            }
+
         }
     }
 }
