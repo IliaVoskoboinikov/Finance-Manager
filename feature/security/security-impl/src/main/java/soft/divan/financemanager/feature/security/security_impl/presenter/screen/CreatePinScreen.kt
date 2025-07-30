b5b5f6231b8891e9dce1b8212d5d98d95d6a8bb4
500a5 <500a5@mail.ru>
2025-07-30 12:10:58 -0700

    fix biometric

--- feature/security/security-impl/src/main/java/soft/divan/financemanager/feature/security/security_impl/presenter/screen/CreatePinScreen.kt
+++ feature/security/security-impl/src/main/java/soft/divan/financemanager/feature/security/security_impl/presenter/screen/CreatePinScreen.kt
@@ -1,63 +1,62 @@
 package soft.divan.financemanager.feature.security.security_impl.presenter.screen
 
-import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import soft.divan.financemanager.feature.security.security_impl.R
 import soft.divan.financemanager.feature.security.security_impl.presenter.data.CreatePinScreenState
-
 import soft.divan.financemanager.feature.security.security_impl.presenter.viewmodel.CreatePinViewModel
 
 
 @Composable
 fun CreatePinScreen(
     viewModel: CreatePinViewModel = hiltViewModel(),
-    onNavegateBack: () -> Unit
+    onNavigateBack: () -> Unit
 ) {
 
 
-    var headerId by remember { mutableStateOf("придумайте пин") }
-    var newPin by remember { mutableStateOf<String?>(null) }
+    var headerId by remember { mutableIntStateOf(R.string.сome_up_with_pin) }
     var tempPin by remember { mutableStateOf<String?>(null) }
-
-    //   val notificationText = stringResource(id = R.string.empty)
-    //  var notification by remember { mutableStateOf(notificationText) }
-    //  val forgotMessageId by remember { mutableIntStateOf(R.string.pin_code_forgot_text) }
-    var isPinEntering by remember { mutableStateOf(false) }
-    var clearTrigger by remember { mutableStateOf(false) }
+    val notificationText = ""
+    var notification by remember { mutableStateOf(notificationText) }
 
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+
     when (val state = uiState) {
 
         is CreatePinScreenState.InitialState -> {
-            headerId = "придумайте пин"
-            isPinEntering = false
+            headerId = R.string.сome_up_with_pin
+            tempPin = null
         }
 
         is CreatePinScreenState.EnteringPinState -> {
-            val pin = (uiState as CreatePinScreenState.EnteringPinState).pin
-            headerId = "повторите пин"
-            tempPin = pin
-            // notification = stringResource(id = R.string.empty)
+            headerId = R.string.repeat_pin
+            tempPin = state.pin
+            notification = ""
+
+
         }
 
         is CreatePinScreenState.ConfirmingPinState -> {
-            if (tempPin == (uiState as CreatePinScreenState.ConfirmingPinState).pin) {
+            if (tempPin == state.pin) {
                 viewModel.savePinCode(tempPin!!)
                 viewModel.changeState(CreatePinScreenState.PinCreatedState)
             } else {
-                // notification = stringResource(id = R.string.pin_codes_do_not_match)
+                notification = stringResource(id = R.string.pin_codes_do_not_match)
                 viewModel.changeState(CreatePinScreenState.InitialState)
             }
+
         }
 
         is CreatePinScreenState.PinCreatedState -> {
-            Log.e("1312ddw", "dd")
-            onNavegateBack()
+            onNavigateBack()
 
         }
 
@@ -67,13 +66,12 @@ fun CreatePinScreen(
 
     }
 
-    PinEntryScreen(
-        title = headerId,
-        clearPinTrigger = clearTrigger,
+    PinEntryCommonScreen(
+        titleId = headerId,
+        errorMessage = notification,
         onPinEntered = { pinCode ->
             if (tempPin == null) {
                 viewModel.changeState(CreatePinScreenState.EnteringPinState(pinCode))
-                clearTrigger = true
             } else {
                 viewModel.changeState(CreatePinScreenState.ConfirmingPinState(pinCode))
             }
