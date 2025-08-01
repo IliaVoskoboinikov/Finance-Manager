3af0f1979ead5cf8d8c832f5357156a116a24c9b
500a5 <500a5@mail.ru>
2025-08-01 10:44:51 -0700

    fix

--- app/src/main/java/soft/divan/financemanager/presenter/MainActivity.kt
+++ app/src/main/java/soft/divan/financemanager/presenter/MainActivity.kt
@@ -65,7 +65,6 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var designAppFeatureApi: DesignAppFeatureApi
 
-
     @Inject
     lateinit var getThemeModeUseCase: GetThemeModeUseCase
 
