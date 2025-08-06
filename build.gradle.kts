e9aed0bb1930288249c283fdbde24d234d066ba0
500a5 <500a5@mail.ru>
2025-08-05 17:35:47 -0700

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,5 +7,7 @@ plugins {
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.graph) apply false
+    alias(libs.plugins.time.tracker) apply false
 
 }
