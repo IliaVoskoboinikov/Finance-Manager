81c31347ecd54fa64e54b746aba9f7cb275c184f
500a5 <500a5@mail.ru>
2025-10-20 15:46:27 +0300

    update transaction

--- feature/transaction/transaction-impl/src/main/java/soft/divan/financemanager/feature/transaction/transaction_impl/domain/usecase/GetTransactionUseCase.kt
+++ feature/transaction/transaction-impl/src/main/java/soft/divan/financemanager/feature/transaction/transaction_impl/domain/usecase/GetTransactionUseCase.kt
@@ -3,5 +3,5 @@ package soft.divan.financemanager.feature.transaction.transaction_impl.domain.us
 import soft.divan.financemanager.core.domain.model.Transaction
 
 interface GetTransactionUseCase {
-    suspend operator fun invoke(id: String): Result<List<Transaction>>
+    suspend operator fun invoke(id: Int): Result<Transaction>
 }
--- feature/transaction/transaction-impl/src/main/java/soft/divan/financemanager/feature/transaction/transaction_impl/navigation/TransactionFeatureImpl.kt
+++ feature/transaction/transaction-impl/src/main/java/soft/divan/financemanager/feature/transaction/transaction_impl/navigation/TransactionFeatureImpl.kt
@@ -3,19 +3,57 @@ package soft.divan.financemanager.feature.transaction.transaction_impl.navigatio
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import soft.divan.financemanager.feature.transaction.transaction_api.TransactionFeatureApi
 import soft.divan.financemanager.feature.transaction.transaction_impl.precenter.screens.TransactionScreen
 import javax.inject.Inject
 
+
+private const val transactionIdKey: String = "transactionId"
+private const val isIncomeKey: String = "isIncome"
+
 class TransactionFeatureImpl @Inject constructor() : TransactionFeatureApi {
+
     override val route: String = "transaction"
+
+    override fun transactionRouteWithArgs(
+        transactionId: Int,
+        isIncome: Boolean
+    ): String {
+        val id = transactionId.toString()
+        val income = isIncome.toString()
+        return "$route/$id/$income"
+    }
 
     override fun registerGraph(
         navGraphBuilder: NavGraphBuilder,
         navController: NavHostController,
         modifier: Modifier
     ) {
+        navGraphBuilder.composable(
+            route = "${route}/{$transactionIdKey}/{$isIncomeKey}",
+            arguments = listOf(
+                navArgument(transactionIdKey) {
+                    type = NavType.IntType
+                },
+                navArgument(isIncomeKey) {
+                    type = NavType.BoolType
+                }
+            )
+        ) { backStackEntry ->
+            val transactionId = backStackEntry.arguments?.getInt(transactionIdKey)
+            val isIncome = backStackEntry.arguments?.getBoolean(isIncomeKey)
+
+            TransactionScreen(
+                modifier = modifier,
+                transactionId = transactionId,
+                isIncome = isIncome,
+                navController = navController
+            )
+        }
+
         navGraphBuilder.composable(route) {
             TransactionScreen(
                 modifier = modifier,
@@ -23,4 +61,6 @@ class TransactionFeatureImpl @Inject constructor() : TransactionFeatureApi {
             )
         }
     }
+
+
 }
