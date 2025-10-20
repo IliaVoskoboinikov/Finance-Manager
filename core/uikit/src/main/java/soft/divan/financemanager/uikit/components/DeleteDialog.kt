aaf633806327ad06105e7e26567916fd72650270
500a5 <500a5@mail.ru>
2025-10-20 18:09:23 +0300

    delete transaction

---
+++ core/uikit/src/main/java/soft/divan/financemanager/uikit/components/DeleteDialog.kt
@@ -0,0 +1,33 @@
+package soft.divan.financemanager.uikit.components
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.res.stringResource
+import soft.divan.financemanager.core.uikit.R
+
+@Composable
+fun DeleteDialog(isShowDeleteDialog: MutableState<Boolean>, onDelete: () -> Unit) {
+    AlertDialog(
+        containerColor = colorScheme.secondaryContainer,
+        onDismissRequest = { isShowDeleteDialog.value = false },
+        confirmButton = {
+            TextButton(onClick = {
+                isShowDeleteDialog.value = false
+                onDelete()
+            }) {
+                Text(stringResource(R.string.delete), color = colorScheme.error)
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = { isShowDeleteDialog.value = false }) {
+                Text(stringResource(R.string.cancel), color = colorScheme.onSecondaryContainer)
+            }
+        },
+        title = { Text(stringResource(R.string.delete_transaction) + "?") },
+        text = { Text(stringResource(R.string.this_action_cannot_be_undone)) }
+    )
+}
