886a3c5f6c7b7992caab1e709ceae02abc8d280b
500a5 <500a5@mail.ru>
2025-11-01 16:14:43 +0300

    delete comment

--- core/domain/src/main/java/soft/divan/financemanager/core/domain/usecase/GetAccountsUseCaseImpl.kt
+++ core/domain/src/main/java/soft/divan/financemanager/core/domain/usecase/GetAccountsUseCaseImpl.kt
@@ -5,17 +5,6 @@ import soft.divan.financemanager.core.domain.model.Account
 import soft.divan.financemanager.core.domain.repository.AccountRepository
 import javax.inject.Inject
 
-/**
- * Реализация бизнес-логики для получения списка всех аккаунтов пользователя.
- *
- * Данный use case инкапсулирует процесс получения аккаунтов,
- * делегируя операцию репозиторию [soft.divan.financemanager.domain.repository.AccountRepository].
- *
- * @property accountRepository репозиторий для работы с аккаунтами
- *
- * @see soft.divan.finansemanager.account.domain.usecase.GetAccountsUseCase
- * @see soft.divan.financemanager.domain.repository.AccountRepository
- */
 class GetAccountsUseCaseImpl @Inject constructor(
     private val accountRepository: AccountRepository
 ) : GetAccountsUseCase {
--- feature/account/account-impl/src/main/java/soft/divan/financemanager/feature/account/account_impl/domain/usecase/impl/UpdateAccountUseCaseImpl.kt
+++ feature/account/account-impl/src/main/java/soft/divan/financemanager/feature/account/account_impl/domain/usecase/impl/UpdateAccountUseCaseImpl.kt
@@ -6,23 +6,10 @@ import soft.divan.financemanager.core.domain.repository.AccountRepository
 import soft.divan.financemanager.feature.account.account_impl.domain.usecase.UpdateAccountUseCase
 import javax.inject.Inject
 
-/**
- * Реализация бизнес-логики для обновления информации об аккаунте.
- *
- * Этот use case инкапсулирует операцию обновления данных аккаунта,
- * делегируя вызов соответствующему методу репозитория [soft.divan.financemanager.domain.repository.AccountRepository].
- *
- * @property accountRepository репозиторий для работы с аккаунтами
- *
- * @see soft.divan.finansemanager.account.domain.usecase.UpdateAccountUseCase
- * @see soft.divan.financemanager.domain.repository.AccountRepository
- */
-
 class UpdateAccountUseCaseImpl @Inject constructor(
     private val accountRepository: AccountRepository
 ) : UpdateAccountUseCase {
     override fun invoke(accountBrief: AccountBrief): Flow<AccountBrief> {
         return accountRepository.updateAccount(accountBrief)
     }
-
 }
