df1ced444609e592a4ff7c4f657ed471fbd0044d
500a5 <500a5@mail.ru>
2025-11-11 15:47:41 +0300

    fix

--- core/uikit/src/main/java/soft/divan/financemanager/uikit/components/ErrorContent.kt
+++ core/uikit/src/main/java/soft/divan/financemanager/uikit/components/ErrorContent.kt
@@ -16,8 +16,18 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import soft.divan.financemanager.core.uikit.R
+import soft.divan.financemanager.uikit.theme.FinanceManagerTheme
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
+@Composable
+fun TransactionScreenPreview() {
+    FinanceManagerTheme {
+        ErrorContent({})
+    }
+}
 
 @Composable
 fun ErrorContent(
