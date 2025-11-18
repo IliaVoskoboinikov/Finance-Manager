3b9696863c0209f0fdbed8db8893a21d0a20ad19
500a5 <500a5@mail.ru>
2025-11-18 15:01:27 +0300

    Analysis feature

---
+++ core/domain/src/main/java/soft/divan/financemanager/core/domain/usecase/GetTransactionByPeriodUseCase.kt
@@ -0,0 +1,15 @@
+package soft.divan.financemanager.core.domain.usecase
+
+import kotlinx.coroutines.flow.Flow
+import soft.divan.financemanager.core.domain.model.Category
+import soft.divan.financemanager.core.domain.model.CurrencyCode
+import soft.divan.financemanager.core.domain.model.Transaction
+import java.time.LocalDate
+
+interface GetTransactionByPeriodUseCase {
+    operator fun invoke(
+        isIncome: Boolean,
+        startDate: LocalDate,
+        endDate: LocalDate
+    ): Flow<Triple<List<Transaction>, CurrencyCode, List<Category>>>
+}
