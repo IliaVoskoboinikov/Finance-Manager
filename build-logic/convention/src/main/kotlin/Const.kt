ff631f03f8275e1a9d29341c59a4e8d9507b6f36
500a5 <500a5@mail.ru>
2025-11-21 14:14:03 +0300

    refactoring build logic

--- build-logic/convention/src/main/kotlin/Const.kt
+++ build-logic/convention/src/main/kotlin/Const.kt
@@ -6,10 +6,3 @@ object Const {
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0"
 }
-
-object Conf {
-    const val IMPLEMENTATION = "implementation"
-    const val DEBUG_IMPLEMENTATION = "debugImplementation"
-    const val KSP = "ksp"
-    const val API = "api"
-}
--- build-logic/convention/src/main/kotlin/HiltConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/HiltConventionPlugin.kt
@@ -1,17 +1,19 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
-import soft.divan.finansemanager.libs
+import soft.divan.finansemanager.Conf
+import soft.divan.finansemanager.applyPlugin
+import soft.divan.finansemanager.lib
 
 class HiltConventionPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         with(project) {
-            pluginManager.apply(libs.findPlugin("hilt").get().get().pluginId)
-            pluginManager.apply(libs.findPlugin("ksp").get().get().pluginId)
+            applyPlugin("hilt")
+            applyPlugin("ksp")
 
             dependencies {
-                add(Conf.IMPLEMENTATION, libs.findLibrary("hilt-android").get())
-                add(Conf.KSP, libs.findLibrary("hilt-compiler").get())
+                add(Conf.IMPLEMENTATION, lib("hilt-android"))
+                add(Conf.KSP, lib("hilt-compiler"))
             }
         }
     }
