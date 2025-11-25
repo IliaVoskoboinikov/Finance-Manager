a9031a42aef0472bbc5ac46f27b981aa5242e1c8
500a5 <500a5@mail.ru>
2025-11-25 14:30:32 +0300

    currency

--- feature/transactions-today/transactions-today-impl/src/main/java/soft/divan/financemanager/feature/transactions_today/transactions_today_impl/di/ExpansesBinderModule.kt
+++ feature/transactions-today/transactions-today-impl/src/main/java/soft/divan/financemanager/feature/transactions_today/transactions_today_impl/di/ExpansesBinderModule.kt
@@ -6,7 +6,7 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import soft.divan.financemanager.feature.transactions_today.transactions_today_api.TransactionsTodayFeatureApi
 import soft.divan.financemanager.feature.transactions_today.transactions_today_impl.domain.GetTodayTransactionsUseCase
-import soft.divan.financemanager.feature.transactions_today.transactions_today_impl.domain.GetTodayTransactionsUseCaseImpl
+import soft.divan.financemanager.feature.transactions_today.transactions_today_impl.domain.impl.GetTodayTransactionsUseCaseImpl
 import soft.divan.financemanager.feature.transactions_today.transactions_today_impl.navigation.TransactionsTodayFeatureImpl
 
 
