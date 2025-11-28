886407f74dbbd6390e87430c4ad559f885602814
500a5 <500a5@mail.ru>
2025-11-28 16:57:28 +0300

    transaction refactoring

---
+++ feature/transaction/transaction-impl/src/main/java/soft/divan/financemanager/feature/transaction/transaction_impl/precenter/model/TransactionEvent.kt
@@ -0,0 +1,9 @@
+package soft.divan.financemanager.feature.transaction.transaction_impl.precenter.model
+
+import androidx.annotation.StringRes
+
+sealed interface TransactionEvent {
+    data object TransactionDeleted : TransactionEvent
+    data object TransactionSaved : TransactionEvent
+    data class ShowError(@field:StringRes val messageRes: Int) : TransactionEvent
+}
