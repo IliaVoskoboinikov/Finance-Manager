457e535c4347936b3cde4a17f577fc7a0a31a117
500a5 <500a5@mail.ru>
2025-12-01 12:09:36 +0300

    category refactoring

--- feature/category/category-impl/src/main/java/soft/divan/financemanager/feature/category/category_impl/navigation/ExpensesFeatureImpl.kt
+++ feature/category/category-impl/src/main/java/soft/divan/financemanager/feature/category/category_impl/navigation/ExpensesFeatureImpl.kt
@@ -9,8 +9,6 @@ import soft.divan.financemanager.feature.category.category_impl.presenter.screen
 import javax.inject.Inject
 
 private const val baseRoute = "category"
-private const val scenarioCategoryRoute = "${baseRoute}/scenario"
-private const val screenCategoryHistoryRoute = "$scenarioCategoryRoute/income_history"
 
 class CategoryFeatureImpl @Inject constructor() : CategoryFeatureApi {
 
@@ -24,10 +22,7 @@ class CategoryFeatureImpl @Inject constructor() : CategoryFeatureApi {
         navGraphBuilder.composable(route) {
             CategoriesScreen(
                 modifier = modifier,
-                navController = navController,
             )
         }
-
-
     }
 }
