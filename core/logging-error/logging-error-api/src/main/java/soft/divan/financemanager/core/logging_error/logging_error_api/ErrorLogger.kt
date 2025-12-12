adc0fef86b55911c96f3da194086d51ea9a802bc
500a5 <500a5@mail.ru>
2025-12-12 18:28:27 +0300

    log error module refactoring

---
+++ core/logging-error/logging-error-api/src/main/java/soft/divan/financemanager/core/logging_error/logging_error_api/ErrorLogger.kt
@@ -0,0 +1,5 @@
+package soft.divan.financemanager.core.logging_error.logging_error_api
+
+interface ErrorLogger {
+    fun recordError(message: String)
+}
--- core/logging/src/main/java/soft/divan/financemanager/core/logging/ErrorLogger.kt
+++
@@ -1,6 +0,0 @@
-package soft.divan.financemanager.core.logging
-
-interface ErrorLogger {
-    fun recordError(message: String)
-}
-
