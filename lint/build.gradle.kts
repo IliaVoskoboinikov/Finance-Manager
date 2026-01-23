9648f1599c04724f1bfecc1b24738aa9804e9bd3
500a5 <500a5@mail.ru>
2026-01-23 18:07:41 +0300

    fix my lint

--- lint/build.gradle.kts
+++ lint/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     `java-library`
     alias(libs.plugins.jetbrains.kotlin.jvm)
@@ -7,6 +9,12 @@ plugins {
 java {
     sourceCompatibility = JavaVersion.VERSION_21
     targetCompatibility = JavaVersion.VERSION_21
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_21
+    }
 }
 
 dependencies {
