2c4555518ef2e18a1b6224d22ce098d6ed295f1b
500a5 <500a5@mail.ru>
2026-02-22 18:04:14 +0300

    test configure release signing and update CD workflow

--- build-logic/convention/src/main/kotlin/Const.kt
+++ build-logic/convention/src/main/kotlin/Const.kt
@@ -3,6 +3,6 @@ object Const {
     const val COMPILE_SKD = 35
     const val MIN_SKD = 26
     const val JAVA_VERSION = "11"
-    const val VERSION_CODE = 3
+    const val VERSION_CODE = 4
     const val VERSION_NAME = "0.0.1"
 }
