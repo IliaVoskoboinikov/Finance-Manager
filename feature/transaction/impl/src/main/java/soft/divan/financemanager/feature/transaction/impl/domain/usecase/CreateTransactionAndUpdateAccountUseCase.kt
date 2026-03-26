4de6cae9baaae699b52a914134d3ce08a2006f92
500a5 <500a5@mail.ru>
2026-03-26 15:58:07 +0300

    implement database transaction support for transaction use cases
    - add `TransactionRunner` interface and `RoomTransactionRunner` implementation to support atomic database operations
    - add `TransactionRollbackException` and `rollbackOnError` extension to handle manual transaction rollbacks
    - rename `CreateTransactionUseCase` to `CreateTransactionAndUpdateAccountUseCase` and refactor to use `TransactionRunner`
    - refactor `UpdateTransactionUseCaseImpl` and `DeleteTransactionUseCaseImpl` to wrap account and transaction operations in a database transaction
    - update `TransactionViewModel` and DI modules to use the renamed use case and new transaction runner
    - add `androidx.room:room-ktx` dependency to `core:data` module

--- feature/transaction/impl/src/main/java/soft/divan/financemanager/feature/transaction/impl/domain/usecase/CreateTransactionAndUpdateAccountUseCase.kt
+++ feature/transaction/impl/src/main/java/soft/divan/financemanager/feature/transaction/impl/domain/usecase/CreateTransactionAndUpdateAccountUseCase.kt
@@ -3,6 +3,6 @@ package soft.divan.financemanager.feature.transaction.impl.domain.usecase
 import soft.divan.financemanager.core.domain.model.Transaction
 import soft.divan.financemanager.core.domain.result.DomainResult
 
-interface CreateTransactionUseCase {
+interface CreateTransactionAndUpdateAccountUseCase {
     suspend operator fun invoke(transaction: Transaction): DomainResult<Unit>
 }
--- feature/transaction/impl/src/main/java/soft/divan/financemanager/feature/transaction/impl/domain/usecase/UpdateTransactionAndUpdateAccountUseCase.kt
+++ feature/transaction/impl/src/main/java/soft/divan/financemanager/feature/transaction/impl/domain/usecase/UpdateTransactionAndUpdateAccountUseCase.kt
@@ -3,6 +3,6 @@ package soft.divan.financemanager.feature.transaction.impl.domain.usecase
 import soft.divan.financemanager.core.domain.model.Transaction
 import soft.divan.financemanager.core.domain.result.DomainResult
 
-interface UpdateTransactionUseCase {
+interface UpdateTransactionAndUpdateAccountUseCase {
     suspend operator fun invoke(transaction: Transaction): DomainResult<Unit>
 }
